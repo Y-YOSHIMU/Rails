cbc6504fe70855dff45e3347af5bc58194b98dc1
Y-YOSHIMU <mcu7.oldtr.uk@gmail.com>
2020-06-06 16:10:33 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '034d8e8a34b101b0bee2bd26fbb0e41f06da0ba8fed2a8b3f80be0c10930a95357f4f004cad773436ef9d2aac3ac01ded71129ebd0e880219d70a05359fd3c88'
